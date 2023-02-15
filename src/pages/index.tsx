8bbe322c7a12b2da7a2fe7ee3631dd2437d753dd
Fossil Frank <fossilfrank@proton.me>
2023-02-15 10:40:11 +0000

    Deposit ETH into FuelV1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,37 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import fuel from '@fuel-js/wallet';
+import { useEffect, useRef, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const wallet = useRef<fuel.Wallet | null>(null);
+  const [balance, setBalance] = useState(0);
+
+  const connect = async () => {
+    // @ts-ignore
+    await window.ethereum.enable();
+  }
+
+  const deposit = async () => {
+    // @ts-ignore
+    const wallet = new fuel.Wallet(window.ethereum, {});
+    await wallet.deposit('0x0000000000000000000000000000000000000000', fuel.utils.parseEther('0.0001'));
+  }
+
+  const updateBalance = async () => {
+    // @ts-ignore
+    const wallet = new fuel.Wallet(window.ethereum, {});
+    const bal = await wallet.balance('0x0000000000000000000000000000000000000000');
+    setBalance(bal);
+  }
+
+  useEffect(() => {
+    updateBalance();
+  }, [])
+
   return (
     <>
       <Head>
@@ -20,43 +47,14 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
         </div>
 
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+          <div>
+            <button onClick={connect}>Connect</button>
+            <button onClick={deposit}>Deposit</button>
           </div>
+          <div>{fuel.utils.formatEther(balance, 'ether')}</div>
         </div>
 
         <div className={styles.grid}>
